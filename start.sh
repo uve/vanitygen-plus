1950b257ddd9ba485364b2f77825be313d81d81d
Nikita Grachev <uve@bounty.local>
2018-01-16 15:06:09 +0700

    test 2 run

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A sample Bash script
 make all
-nohup ./oclvanitygen -D 0:0 -1 -o Address.txt -f wallets.txt > logs.txt &
+nohup ./oclvanitygen -D 0:0 -1 -o Address.txt -f wallets.txt > logs.txt 2>&1 &
