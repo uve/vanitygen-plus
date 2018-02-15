c866608acde79bfc29fff99667f3bd31d725fd4c
Nikita Grachev <nikita.grachev@gmail.com>
2018-02-15 19:53:41 +0700

    fixed run command

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A sample Bash script
 make all
-nohup ./oclvanitygen -D 0:0 -1 -o Address.txt -f wallets.txt -o results.txt > logs.txt 2>&1 &
+nohup ./oclvanitygen -D 0:0 -1 -o Address.txt -f wallets.txt > logs.txt 2>&1 &
